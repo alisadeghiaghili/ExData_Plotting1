74bd49d13fd731794128c7c3ef8aab84ae12e6a0
Ali Sadeghi Aghili <alisadeghiaghili@gmail.com>
2022-09-11 20:37:13 +0430

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,22 @@
+data <- read.table(file = "./household_power_consumption.txt", 
+                        colClasses = c(rep("character", 2), rep("numeric", 7)),
+                        stringsAsFactors = FALSE, header = TRUE, 
+                        sep =";",
+                        na.string = "?")
+
+data$Date <- strptime(x = data$Date, format = "%d/%m/%Y")
+
+twoDays <- subset(data, Date == "2007-02-01" | Date =="2007-02-02")
+
+rm(data)
+
+png(filename = "plot1.png", width = 480, height = 480)
+
+hist(twoDays$Global_active_power, 
+     col = "red", 
+     border = "black",
+     xlab = "Global Active Power (kilowatts)",
+     ylab = "Frequency", 
+     main = "Global Active Power")
+
+dev.off()
